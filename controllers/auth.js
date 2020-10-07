1150bcf4060f947fb6e878182879630850acff7d
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2020-10-07 21:19:44 +0200

    Refactored promises to async

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken')
 
 const User = require('../models/user')
 
-exports.signup = (req, res, next) => {
+exports.signup = async(req, res, next) => {
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
     const error = new Error('Validation failed')
@@ -16,46 +16,44 @@ exports.signup = (req, res, next) => {
   const name = req.body.name
   const password = req.body.password
 
-  bcrypt.hash(password, 12)
-  .then(hashedPassword => {
+  const hashedPassword = await bcrypt.hash(password, 12)
+  try {
     const user = new User({
       email: email,
       password: hashedPassword,
       name: name
     })
-    return user.save()
-  })
-  .then(result => {
+    const result = await user.save()
     res.status(201).json({
       message: 'User created!',
       userId: result._id
     })
-  })
-  .catch(err => {
+  } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500
     }
     next(err)
-  })
+  }
 }
 
-exports.login = (req, res, next) => {
+exports.login = async(req, res, next) => {
   const email = req.body.email
   const password = req.body.password
   let loadedUser
-  User.findOne({
-    email: email
-  })
-  .then(user => {
+
+  try {
+    const user = await User.findOne({
+      email: email
+    })
     if (!user) {
       const error = new Error('No user found')
       error.statusCode = 401
       throw error
     }
     loadedUser = user
-    return bcrypt.compare(password, user.password)
-  })
-  .then(isEqual => {
+
+    const isEqual = await bcrypt.compare(password, user.password)
+
     if (!isEqual) {
       const error = new Error('Wrong password')
       error.statusCode = 401
@@ -73,18 +71,17 @@ exports.login = (req, res, next) => {
       token: token,
       userId: loadedUser._id.toString()
     })
-  })
-  .catch(err => {
+  } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500
     }
     next(err)
-  })
+  }
 }
 
-exports.getUserStatus = (req, res, next) => {
-  User.findById(req.userId)
-  .then(user => {
+exports.getUserStatus = async(req, res, next) => {
+  try {
+    const user = await User.findById(req.userId)
     if (!user) {
       const error = new Error('User not found')
       error.statusCode = 404
@@ -93,36 +90,32 @@ exports.getUserStatus = (req, res, next) => {
     res.status(200).json({
       status: user.status
     })
-  })
-  .catch(err => {
+  } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500
     }
     next(err)
-  })
+  }
 }
 
-exports.updateUserStatus = (req, res, next) => {
+exports.updateUserStatus = async(req, res, next) => {
   const newStatus = req.body.status
-  User.findById(req.userId)
-  .then(user => {
+  try {
+    const user = await User.findById(req.userId)
     if (!user) {
       const error = new Error('User not found')
       error.statusCode = 404
       throw error
     }
     user.status = newStatus
-    return user.save()
-  })
-  .then(result => {
+    await user.save()
     res.status(200).json({
       message: 'User updated!'
     })
-  })
-  .catch(err => {
+  } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500
     }
     next(err)
-  })
+  }
 }
