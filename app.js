0e6e3dfab152e9334d6c952183f8d9792b9c27d1
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2020-10-08 16:55:14 +0200

    Established server to client connection for real time updates via websocket

--- app.js
+++ app.js
@@ -70,7 +70,11 @@ mongoose
   process.env.DATABASE
 )
 .then(result => {
-  app.listen(8080)
+  const server = app.listen(8080)
+  const io = require('./socket').init(server)
+  io.on('connection', socket => {
+    console.log('Client connected')
+  })
 })
 .catch(err => {
   console.log(err)
--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken')
 
+
+
 module.exports = (req, res, next) => {
   const authHeader = req.get('Authorization')
   if (!authHeader) {
@@ -10,7 +12,7 @@ module.exports = (req, res, next) => {
   const token = authHeader.split(' ')[1]
   let decodedToken
   try {
-    decodedToken = jwt.verify(token, 'C6Q678VTX737447D')
+    decodedToken = jwt.verify(token, process.env.TOKEN)
   } catch (err) {
     err.statusCode = 500
     throw err
