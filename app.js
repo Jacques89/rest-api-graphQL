767d98ad5bc6e242b92d1aaa12be234a9b0d5f69
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2020-11-24 13:19:58 +0100

    added a mutation resolver and GraphiQL

--- app.js
+++ app.js
@@ -54,7 +54,8 @@ app.use(
   '/graphql', 
   graphqlHTTP({
     schema: graphqlSchema,
-    rootValue: graphqlResolver
+    rootValue: graphqlResolver,
+    graphiql: true
   }
 ))
 
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,3 +1,36 @@
+const bcrypt = require('bcryptjs')
+const validator = require('validator')
+
+const User = require('../models/user')
+
 module.exports = {
-  
+  createUser: async function({ userInput }, req) {
+    const errors = []
+    if (!validator.isEmail(userInput.email)) {
+      errors.push({ message: 'Invalid E-mail' })
+    }
+    if (
+      validator.isEmpty(userInput.password) || 
+      !validator.isLength(userInput.password, { min: 5 })
+    ) {
+      errors.push({ message: 'Password is too short!' })
+    }
+    if (errors.length > 0) {
+      const error = new Error('Invalid input.')
+      throw error
+    }
+    const existingUser = await User.findOne({email: userInput.email})
+    if (existingUser) {
+      const error = new Error('User already exists')
+      throw error
+    }
+    const hashedPassword = await bcrypt.hash(userInput.password, 12)
+    const user = new User({
+      email: userInput.email,
+      name: userInput.name,
+      password: hashedPassword
+    })
+    const createdUser = await user.save()
+    return { ...createdUser._doc, _id: createdUser._id.toString() }
+  }
 }
