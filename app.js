137f6fe3467d9fb42626e36daf568878c2e31886
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2020-11-24 14:25:11 +0100

    connected the frontend to the backend

--- app.js
+++ app.js
@@ -47,6 +47,9 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200)
+  }
   next()
 })
 
@@ -55,7 +58,20 @@ app.use(
   graphqlHTTP({
     schema: graphqlSchema,
     rootValue: graphqlResolver,
-    graphiql: true
+    graphiql: true,
+    customFormatErrorFn(err) {
+      if (!err.originalError) {
+        return err
+      }
+      const data = err.originalError.data
+      const message = err.message || 'An error occured'
+      const code = err.originalError.code || 500
+      return {
+        message: message,
+        status: code,
+        data: data
+      }
+    }
   }
 ))
 
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -17,9 +17,11 @@ module.exports = {
     }
     if (errors.length > 0) {
       const error = new Error('Invalid input.')
+      error.data = errors
+      error.code = 422
       throw error
     }
-    const existingUser = await User.findOne({email: userInput.email})
+    const existingUser = await User.findOne({ email: userInput.email })
     if (existingUser) {
       const error = new Error('User already exists')
       throw error
